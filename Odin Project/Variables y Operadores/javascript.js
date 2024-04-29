df63a2fe684be8f18f5dcc6a6697f04d27fe7d28
Andy-Castle <masterchief_and@live.com>
2024-04-28 20:02:27 -0500

    variables y operadores de javascript

---
+++ Odin Project/Variables y Operadores/javascript.js
@@ -0,0 +1,68 @@
+// let message;
+
+// message = "Hello";
+
+// alert(message);
+
+// let user = "John";
+// let age = 25;
+// let message = "Hello";
+
+// let message;
+
+// message = "Hello";
+
+// message = "World!";
+
+// alert(message);
+
+// let hello = "Hello world!";
+
+// let message;
+
+// message = hello;
+
+// alert(hello);
+
+// alert(message);
+
+// const myBirthday = "30.08.1999";
+
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+
+// let color = COLOR_ORANGE;
+
+// alert(color);
+
+// let admin;
+// let name;
+
+// name = "Jhon";
+
+// admin = name;
+
+// alert(admin);
+
+// let nameOfOurPlanet = "Earth";
+// let nameOfOurCurrentVisitor = "Jhon";
+
+console.log(23 + 97 + 32 + 54 + 454 + 865);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10;
+
+let b = 7 * a;
+
+console.log(b);
+
+const MAX = 57;
+
+let actual = MAX - 13;
+
+let percentage = actual / MAX;
+
+console.log(percentage);
