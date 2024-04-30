5efde161e1b4d803b87fb8df9ee2afa6b5e3e3d2
Andy-Castle <masterchief_and@live.com>
2024-04-29 19:45:33 -0500

    hay que ver el video de youtube de solución de problemas

---
+++ Odin Project/Solución de Problemas/try.js
@@ -0,0 +1,12 @@
+// When the user inputs a number
+// Initialize a counter variable and set its value to zero
+// While counter is smaller than user inputted number increment the counter by one
+// Print the value of the counter variable
+
+let userInput = 10;
+let counter = 0;
+
+while (counter < userInput) {
+  counter++;
+  console.log(counter);
+}
